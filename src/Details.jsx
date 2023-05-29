bc1c2df7fee34d5551d1971dff2e9b2c0c381767
Michael <lemichael1215@gmail.com>
2023-05-29 15:26:24 +0300

    Added the Carousel component to the Details.jsx

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import fetchPet from "./fetchPet";
+import Carousel from "./Carousel";
 
 const Details = () => {
   const { id } = useParams();
@@ -19,7 +20,7 @@ const Details = () => {
   return (
     <div className="details">
       <div>
-        <h1>{pet.name}</h1>
+        <Carousel images={pet.images} />;<h1>{pet.name}</h1>
         <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
         <button>Adopt {pet.name}</button>
         <p>{pet.description}</p>
