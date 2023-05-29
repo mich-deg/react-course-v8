a8443e5adb119b93fcae402f66cde8a77d13277b
Michael <lemichael1215@gmail.com>
2023-05-29 13:37:10 +0300

    updated Details.jsx and correction on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const queryClient = new QueryClient({
 const App = () => {
   return (
     <BrowserRouter>
-      <QueryClientProvider queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
         <header>
           <Link to="/">Adopt Me!</Link>
         </header>
